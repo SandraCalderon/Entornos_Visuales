8ccec725d08fd43151800fdb260af0537120ced8
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-26 08:22:42 -0600

    ejercicio combobox clase

---
+++ comboBoxItems/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmbEquipos = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(80, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Mostrar"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmbEquipos
+        '
+        Me.cmbEquipos.FormattingEnabled = True
+        Me.cmbEquipos.Items.AddRange(New Object() {"Sandra", "Rigoberto", "Jackelin", "Ciclista", "Cardona"})
+        Me.cmbEquipos.Location = New System.Drawing.Point(55, 63)
+        Me.cmbEquipos.Name = "cmbEquipos"
+        Me.cmbEquipos.Size = New System.Drawing.Size(121, 21)
+        Me.cmbEquipos.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 256)
+        Me.Controls.Add(Me.cmbEquipos)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents cmbEquipos As ComboBox
+End Class
---
+++ comboBoxItems/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim itemId As Integer
+        Dim itemNombre As String
+
+        itemId = cmbEquipos.SelectedIndex
+        itemNombre = cmbEquipos.SelectedItem.ToString
+
+        Select Case itemId
+            Case 0
+                MsgBox(itemNombre)
+            Case 2
+                MsgBox(itemNombre)
+            Case 2
+                MsgBox(itemNombre)
+            Case 3
+                MsgBox(itemNombre)
+            Case 4
+                MsgBox(itemNombre)
+            Case 5
+                MsgBox(itemNombre)
+            Case Else
+                MsgBox("Seleccione un valor corrrecto")
+
+        End Select
+    End Sub
+End Class
