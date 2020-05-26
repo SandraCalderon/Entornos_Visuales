74e6d8612a5745b35045cefa5e5db261ff6f5a19
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-26 08:54:39 -0600

    botonValidar Completo

---
+++ ErrorProvide/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnValidar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProducto = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ErrordeValidacion = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.ErrordeValidacion, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnValidar
+        '
+        Me.btnValidar.Location = New System.Drawing.Point(59, 328)
+        Me.btnValidar.Name = "btnValidar"
+        Me.btnValidar.Size = New System.Drawing.Size(75, 23)
+        Me.btnValidar.TabIndex = 0
+        Me.btnValidar.Text = "Validar"
+        Me.btnValidar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(57, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Producto"
+        '
+        'txtProducto
+        '
+        Me.txtProducto.Location = New System.Drawing.Point(127, 48)
+        Me.txtProducto.Name = "txtProducto"
+        Me.txtProducto.Size = New System.Drawing.Size(100, 20)
+        Me.txtProducto.TabIndex = 2
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(127, 97)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(57, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Precio"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(127, 152)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(57, 154)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Cantidad"
+        '
+        'ErrordeValidacion
+        '
+        Me.ErrordeValidacion.ContainerControl = Me
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(127, 195)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(57, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Descripcion"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(632, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtProducto)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnValidar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.ErrordeValidacion, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnValidar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProducto As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ErrordeValidacion As ErrorProvider
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ ErrorProvide/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub btnValidar_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+        'try catch
+        'validarteChildre buttonm 
+        'validating(Dentro de las cajas de texto)
+        'validated nos valida si dejamos un caja vacia
+        Try
+            'si las casills estas sin ningun valor o vacias
+            'primero valida que producto<> vacio, precio<>vacio, cantidad val(cantidad)convierte ese valor en entero
+            If Me.ValidateChildren And txtProducto.Text <> String.Empty And txtPrecio.Text <> String.Empty And Val(txtCantidad.Text) - Int(Val(txtCantidad.Text)) = 0 Then
+                'me. accesede a todos los datos y validateChildren se lo aplica a todos los controles
+                'String.Empty es para verificar si esta vacio es igual a decir " " 
+                '= para que solo reconozca cuando este vacio
+                MessageBox.Show("Producto Ingresado", "Formulario Producto", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Revise los datos ingresados", "Error en productos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+        Catch ex As Exception
+            'catch ex as exception
+            'catch variables as excepcion
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+
+    Private Sub txtProducto_Validated(sender As Object, e As EventArgs) Handles txtProducto.Validated
+
+    End Sub
+
+
+    Private Sub txtPrecio_Validated(sender As Object, e As EventArgs) Handles txtPrecio.Validated
+
+    End Sub
+End Class
