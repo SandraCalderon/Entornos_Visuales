19061efd424a8a5efeea10c2550887b1f62167e0
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-30 21:14:32 -0600

    suma de dos numeros ejercicio

---
+++ SumadeDosNumeros/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.txtValor2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSuma = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(75, 111)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 2
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Valor 1"
+        '
+        'txtValor1
+        '
+        Me.txtValor1.Location = New System.Drawing.Point(119, 22)
+        Me.txtValor1.Name = "txtValor1"
+        Me.txtValor1.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor1.TabIndex = 0
+        '
+        'txtValor2
+        '
+        Me.txtValor2.Location = New System.Drawing.Point(119, 62)
+        Me.txtValor2.Name = "txtValor2"
+        Me.txtValor2.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Valor 2"
+        '
+        'txtSuma
+        '
+        Me.txtSuma.Enabled = False
+        Me.txtSuma.Location = New System.Drawing.Point(119, 171)
+        Me.txtSuma.Name = "txtSuma"
+        Me.txtSuma.Size = New System.Drawing.Size(100, 20)
+        Me.txtSuma.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 174)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Suma"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(169, 111)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 3
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(317, 229)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.txtSuma)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtValor2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtValor1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Suma de dos numeros"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtValor1 As TextBox
+    Friend WithEvents txtValor2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSuma As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSalir As Button
+End Class
