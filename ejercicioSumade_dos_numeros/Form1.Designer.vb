26514b3ffa9c7a72369221f66f41e0d9afa3aeb7
SandraCalderon <sandra21jackelin@gmail.com>
2020-05-31 18:25:44 -0600

    suma de dos numeros completo

--- ejercicioSumade_dos_numeros/Form1.Designer.vb
+++ ejercicioSumade_dos_numeros/Form1.Designer.vb
@@ -22,17 +22,109 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtSuma = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtValor2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'txtSuma
+        '
+        Me.txtSuma.Enabled = False
+        Me.txtSuma.Location = New System.Drawing.Point(85, 158)
+        Me.txtSuma.Name = "txtSuma"
+        Me.txtSuma.Size = New System.Drawing.Size(100, 20)
+        Me.txtSuma.TabIndex = 15
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Suma"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(130, 108)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 13
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(33, 108)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 12
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'txtValor1
+        '
+        Me.txtValor1.Location = New System.Drawing.Point(85, 58)
+        Me.txtValor1.Name = "txtValor1"
+        Me.txtValor1.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor1.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Valor2"
+        '
+        'txtValor2
+        '
+        Me.txtValor2.Location = New System.Drawing.Point(85, 22)
+        Me.txtValor2.Name = "txtValor2"
+        Me.txtValor2.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor2.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Valor 1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(455, 302)
+        Me.ClientSize = New System.Drawing.Size(237, 201)
+        Me.Controls.Add(Me.txtSuma)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtValor1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtValor2)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "suma de dos numeros"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtSuma As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtValor1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtValor2 As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- ejercicioSumade_dos_numeros/Form1.vb
+++ ejercicioSumade_dos_numeros/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim valor1, valor2, suma As Integer
+
+        valor1 = Val(txtValor1.Text)
+        valor2 = Val(txtValor2.Text)
+
+        suma = valor1 + valor2
+
+        txtSuma.Text = suma
+
+    End Sub
 End Class
